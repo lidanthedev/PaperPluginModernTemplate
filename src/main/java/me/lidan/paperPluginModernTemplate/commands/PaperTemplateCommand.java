d0332d694736fa1aaab9e164ad0d2f89d580ba72
lidanthedev <ls301205@gmail.com>
2026-01-05 21:03:37 +0200

    Added Permission for the template command

--- src/main/java/me/lidan/paperPluginModernTemplate/commands/PaperTemplateCommand.java
+++ src/main/java/me/lidan/paperPluginModernTemplate/commands/PaperTemplateCommand.java
@@ -3,8 +3,10 @@ package me.lidan.paperPluginModernTemplate.commands;
 import org.bukkit.command.CommandSender;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.Subcommand;
+import revxrsal.commands.bukkit.annotation.CommandPermission;
 
 @Command("papertemplate")
+@CommandPermission("papertemplate.use")
 public class PaperTemplateCommand {
     @Subcommand("hello")
     public void hello(CommandSender sender) {
